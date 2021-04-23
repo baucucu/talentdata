5f8e7b7ecc6cfcfa22b7ffa73498ab584ee5f928
baucucu <alexandru.raduca@gmail.com>
2021-04-23 08:37:15 +0300

    change candidate status implemented

--- src/pages/Candidates.jsx
+++ src/pages/Candidates.jsx
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+// import { f7ready } from 'framework7-react';
 import { Button, List, ListItem, AccordionContent, Toggle, Col, Chip, Icon, Link, Page, Card, CardHeader, CardFooter, CardContent ,Navbar, Block, BlockTitle, Row, Segmented} from 'framework7-react';
 import mongodb from '../js/mongodb';
 
@@ -10,9 +11,33 @@ export default function Candidates({ f7route }) {
     const db = mongodb.db("candidates")
     const coll = db.collection(f7route.query.collection)
     
-    useEffect(async () => {
+    const fetchCandidates = async (coll) => {
         const records = await coll.find()
         setCandidates(records)
+    }
+
+    const fetchCandidate = async (coll) => {
+        const record = await coll.findOne()
+        console.log(record)
+    }
+
+    const updateCandidate = async (coll, _id, status) => {
+        const query = {"_id": _id};
+        const update = {
+            "$set": {
+                "Status" : status
+            }
+        };
+        const options = { "upsert": false };
+        const result = await coll.updateOne(query, update)
+        fetchCandidates(coll)
+        console.log("status updated: ",status)
+        return result
+    }
+    
+    useEffect(() =>{
+        fetchCandidates(coll)
+        fetchCandidate(coll)
     },[])
 
     return (
@@ -25,7 +50,14 @@ export default function Candidates({ f7route }) {
             <Row>
                 {candidates.map(candidate => {
                     return(
-                        <CandidateCard key={candidate["Public LinkedIn URL"]} candidate={candidate}/>
+                        <CandidateCard 
+                            key={candidate["Public LinkedIn URL"]} 
+                            candidate={candidate}
+                            fetchCandidate={fetchCandidate}
+                            fetchCandidates={fetchCandidates}
+                            updateCandidate={updateCandidate}
+                            coll={coll}
+                        />
                     )
                 })}
             </Row>
@@ -35,11 +67,17 @@ export default function Candidates({ f7route }) {
 }
 
 const CandidateCard = (props) => {
+
+    const [candidate, setCandidate] = useState(props.candidate)
+
+    const {updateCandidate, fetchCandidates, coll} = props
     const firstName = props.candidate["Name"].split(" ")[0]
     
-    const stars = props.candidate["Candidate Ranking"] //[Object.keys(props.candidate["Candidate Ranking"])[0]]
-    // console.log(props.candidate)
+    const stars = props.candidate["Candidate Ranking"]
+    const [status, setStatus] = useState(props.candidate["Status"])
     
+    
+
     return(
         <Card style={{width: "360px"}}>
             <CardHeader >
@@ -55,39 +93,120 @@ const CandidateCard = (props) => {
                     <img src={props.candidate["Photo-src"]} width="100%" alt="profile"/>
                 </Col>
             </CardContent >
-            <CardContent >
+            {props.candidate["Language 1"] !== "null" && <CardContent >
                 <Block>
-                    {props.candidate["Language 1"] !== "null" && <Chip>{props.candidate["Language 1"]}</Chip>}
-                    {props.candidate["Language 2"] !== "null" && <Chip>{props.candidate["Language 2"]}</Chip>}
-                    {props.candidate["Language 3"] !== "null" && <Chip>{props.candidate["Language 3"]}</Chip>}
-                    {props.candidate["Language 4"] !== "null" && <Chip>{props.candidate["Language 4"]}</Chip>}
+                    <Chip style={{marginRight:"4px", fontSize:"11px"}}>{props.candidate["Language 1"]}</Chip>
+                    {props.candidate["Language 2"] !== "null" && <Chip style={{marginRight:"4px", fontSize:"11px"}}>{props.candidate["Language 2"]}</Chip>}
+                    {props.candidate["Language 3"] !== "null" && <Chip style={{marginRight:"4px", fontSize:"11px"}}>{props.candidate["Language 3"]}</Chip>}
+                    {props.candidate["Language 4"] !== "null" && <Chip style={{marginRight:"4px", fontSize:"11px"}}>{props.candidate["Language 4"]}</Chip>}
+                </Block>
+            </CardContent>}
+            <CardContent>
+                <Block>
+                    {props.candidate["Standardised position"] && <Chip outline style={{marginRight:"4px", fontSize:"11px"}}>{props.candidate["Standardised position"]}</Chip>}
+                    <Chip outline style={{marginRight:"4px", fontSize:"11px"}}>{props.candidate.industry}</Chip>
+                    {props.candidate?.["Functional recruitment area"] && <Chip outline style={{marginRight:"4px", fontSize:"11px"}}>{props.candidate["Functional recruitment area"]}</Chip>}
+
                 </Block>
             </CardContent>
             {props.candidate["Summary"] !== "" && <CardContent width="300px">
                     <List accordionList>
                         <ListItem accordionItem title="Summary">
                             <AccordionContent>
-                            <Block>
-                                {props.candidate["Summary"]}
-                            </Block>
+                                <Block>
+                                    {props.candidate["Summary"]}
+                                </Block>
                             </AccordionContent>
                         </ListItem>
                     </List>
             </CardContent>}
+            <CardContent>
+                <List accordionList>
+                    <ListItem accordionItem title="Experience">
+                        <AccordionContent>
+                            <List mediaList>
+                                <ListItem 
+                                    title={props.candidate["Current Employer"]}
+                                    text={props.candidate["Current Title"]} 
+                                    after= {props.candidate["Tenure"]} 
+                                >
+                                </ListItem>
+                                <ListItem 
+                                    title={props.candidate["Previous employer 2"]}
+                                    text={props.candidate["Previous Title 2"]} 
+                                    after={props.candidate["2nd company length in role" ]} 
+                                >
+                                </ListItem>
+                                <ListItem  
+                                    title={props.candidate["Previous employer 3"]}
+                                    text={props.candidate["Previous Title 3"]} 
+                                    after={props.candidate["3rd Company length in role" ]} 
+                                >
+                                </ListItem>
+                                <ListItem  
+                                    title={props.candidate["4th Company"]}
+                                    text={props.candidate["4th Position"]} 
+                                    after={props.candidate["4th Company Length in role" ]}   
+                                >
+                                </ListItem>
+                            </List>
+                        </AccordionContent>
+                    </ListItem>
+                </List>
+            </CardContent>
+            {props.candidate["Top University Attended"] !== "null" && <CardContent>
+                <List accordionList>
+                    <ListItem accordionItem title="Education">
+                        <AccordionContent>
+                            <List mediaList>
+                                <ListItem 
+                                    title={props.candidate["Top University Attended"]}
+                                    text={props.candidate["Top Degree obtained"]} 
+                                    after= {props.candidate["Top Degree Dates attended"]} 
+                                >
+                                </ListItem>
+                                {props.candidate["2nd University Attended"] !== "null" && <ListItem 
+                                    title={props.candidate["2nd University Attended"]}
+                                    text={props.candidate["2nd Degree obtained"]} 
+                                    after= {props.candidate["2nd University Dates Attended"]} 
+                                ></ListItem>}
+                                {props.candidate["3rd University Attended"] !== "null" && <ListItem 
+                                    title={props.candidate["3rd University Attended"]}
+                                    text={props.candidate["3rd Degree Obtained"]} 
+                                    after= {props.candidate["3rd Degree Dates Attended"]} 
+                                ></ListItem>}
+                            </List>
+                        </AccordionContent>
+                    </ListItem>
+                </List>
+            </CardContent>}
             <CardFooter className="no-border" style={{marginTop:"20px"}}>
-                <Col>
-                    <Segmented  raised tag="p">
-                        <Button   color="green">
-                            Approved
+                    <Segmented  raised tag="p" style={{width:"100%"}}>
+                        <Button 
+                            color="green" 
+                            active={status === "approved"}
+                            onClick={() => {updateCandidate(coll, props.candidate._id, "approved"); fetchCandidates(coll); console.log("approved")}}
+                        >
+                            <Icon f7="hand_thumbsup_fill" size="14px" style={{marginRight:"8px"}}></Icon>
+                            {status === "approved" ? "Approved" : "Approve"}
                         </Button>
-                        <Button  active color="blue">
+                        <Button 
+                            color="deeporange" 
+                            active={status === ""}
+                            onClick={() => {updateCandidate(coll, props.candidate._id, ""); console.log("pending")}}
+                        >
+                            <Icon f7="pause_circle_fill" size="14px" style={{marginRight:"8px"}}></Icon>
                             Pending
                         </Button>
-                        <Button  color="red">
-                            Rejected
+                        <Button  
+                            color="red" 
+                            active={status === "rejected"}
+                            onClick={() => {updateCandidate(coll, props.candidate._id, "rejected"); console.log("rejected")}}
+                        >
+                            <Icon f7="hand_thumbsdown_fill" size="14px" style={{marginRight:"8px"}}></Icon>
+                            {status === "rejected" ? "Rejected" : "Reject"}
                         </Button>
                     </Segmented>
-                </Col>
             </CardFooter>
         </Card>
     )
